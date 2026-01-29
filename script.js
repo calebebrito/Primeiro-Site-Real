4a2439cdb1b5a1b1967f8a2b6bc821e41184c716
Calebe Ribeiro <ribeirozx15@gmail.com>
2026-01-29 15:45:41 -0300

    Versus 1.8

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const menuButton = document.getElementById("menuButton")
 
 menuButton.addEventListener("click", function() {
     whitePage.classList.toggle("open")
+    menuButton.classList.toggle("open")
 })
