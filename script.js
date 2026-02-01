d7d9f144fb8f95515866887d4df202e9ccc1c080
Calebe Ribeiro <ribeirozx15@gmail.com>
2026-02-01 17:11:13 -0300

    Versus 2.0

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const whitePage = document.getElementById("menuPage")
 const menuButton = document.getElementById("menuButton")
 
-menuButton.addEventListener("click", function() {
+menuButton.addEventListener("click", function(openMenu) {
     whitePage.classList.toggle("open")
     menuButton.classList.toggle("open")
+    overlay.classList.toggle("show")
 })
+
+document.addEventListener("click", (event) => {
+    const clickedInsideMenu = menuButton.contains(event.target)
+
+    if (!clickedInsideMenu) {
+        whitePage.classList.remove("open")
+        menuButton.classList.remove("open")
+        overlay.classList.remove("show")
+    }
+})
+
+whitePage.addEventListener("click", (e) => {
+    e.stopPropagation();
+});
